17da8bc0f8443d4d59d8f257368f40aa55c79320
Dilip Kumar K <dilipkundrapu123@gmail.com>
2018-01-17 11:16:15 +0530

    Add files via upload

---
+++ static/js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
